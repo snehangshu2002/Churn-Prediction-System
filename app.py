01838316db0c4472b3fe0f5ba83d4ebb9651f800
snehangshu <snehangshubhuin@gmail.com>
2025-10-22 00:39:37 +0530

    Data Ingestion is completed

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from src.Customer_Churn_Prediction.logger import logging
 from src.Customer_Churn_Prediction.exception import CustomException
 import sys
-
-
+from src.Customer_Churn_Prediction.components.data_ingestion import DataIngestion,DataIngestionConfig
 if __name__=="__main__":
     logging.info("The execution has started")
 
     try:
-        a=1/0
+        #data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
