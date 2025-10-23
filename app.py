a1f78d638b5e4d67682a5fbd50c9d8804a464256
snehangshu <snehangshubhuin@gmail.com>
2025-10-24 00:33:18 +0530

    Data trandformation

--- app.py
+++ app.py
@@ -2,13 +2,24 @@ from src.Customer_Churn_Prediction.logger import logging
 from src.Customer_Churn_Prediction.exception import CustomException
 import sys
 from src.Customer_Churn_Prediction.components.data_ingestion import DataIngestion,DataIngestionConfig
+from src.Customer_Churn_Prediction.components.data_transformation import DataTransformationConfig,DataTransformation
 if __name__=="__main__":
     logging.info("The execution has started")
 
     try:
         #data_ingestion_config=DataIngestionConfig()
         data_ingestion=DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+
+        #data_transformation_config=DataTransformationConfig()
+        data_transformation=DataTransformation()
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+        # ## Model Training
+
+        # model_trainer=ModelTrainer()
+        # print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+        
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
