eed34bb503de2a8485a92cd4251f3452476d5534
snehangshu <snehangshubhuin@gmail.com>
2025-11-05 13:54:26 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,33 @@
+'''
+The setup.py file is an essential part of packaging and 
+distributing Python projects. It is used by setuptools 
+(or distutils in older Python versions) to define the configuration 
+of your project, such as its metadata, dependencies, and more
+'''
+
 from setuptools import find_packages,setup
 from typing import List
 
+def get_requirements()->List[str]:
+    """
+    This function will return list of requirements
+    
+    """
+    requirement_lst:List[str]=[]
+    try:
+        with open('requirements.txt','r') as file:
+            #Read lines from the file
+            lines=file.readlines()
+            ## Process each line
+            for line in lines:
+                requirement=line.strip()
+                ## ignore empty lines and -e .
+                if requirement and requirement!= '-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found")
 
-# HYPEN_E_DOT="-e ."
-
-def get_requirements(file_path:str)-> List[str]:
-    '''
-    this function will return the list of requirements
-    '''
-    requirements=[]
-
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
-         
-        # if HYPEN_E_DOT in requirements:
-        #     requirements.remove(HYPEN_E_DOT)
-
-    return requirements
+    return requirement_lst
 
 
 setup(
@@ -27,7 +37,7 @@ setup(
     author="Snehangshu Bhuin",
     author_email="snehangshubhuin@gmail.com",
     package=find_packages(),
-    install_requires=get_requirements('requirements.txt')
+    install_requires=get_requirements()
 
 
 
